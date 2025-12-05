b58fa096912558493beb94d66c35322d973ff819
Dairon Rodriguez <dairolrpichardo90@yahoo.com>
2025-12-04 21:31:32 -0500

    Update CallSecurityScreeningService.kt

--- app/src/main/java/com/callsecurity/agent2/service/CallSecurityScreeningService.kt
+++ app/src/main/java/com/callsecurity/agent2/service/CallSecurityScreeningService.kt
@@ -4,16 +4,21 @@ import android.telecom.Call
 import android.telecom.CallScreeningService
 import android.telecom.Call.Details
 import android.util.Log
-import com.callsecurity.agent2.core.SpamEngine
+import com.callsecurity.agent2.core.CallClassifier
 
-class CallShieldScreeningService : CallScreeningService() {
+class CallSecurityScreeningService : CallScreeningService() {
+
+    private val classifier = CallClassifier()
 
     override fun onScreenCall(callDetails: Details) {
         try {
             val phoneNumber = callDetails.handle.schemeSpecificPart ?: ""
             Log.d("CallShield", "Incoming call: $phoneNumber")
 
-            val isSpam = SpamEngine.isSpamNumber(phoneNumber)
+            val isSpam = classifier.isSpam(phoneNumber)
+            val score = classifier.classificationScore(phoneNumber)
+
+            Log.d("CallShield", "Spam: $isSpam (Score $score)")
 
             val response = CallResponse.Builder()
                 .setDisallowCall(isSpam)
@@ -26,7 +31,7 @@ class CallShieldScreeningService : CallScreeningService() {
             respondToCall(callDetails, response)
 
         } catch (e: Exception) {
-            Log.e("CallShield", "CallScreeningService error", e)
+            Log.e("CallShield", "Error during screening: ${e.message}")
         }
     }
 }
