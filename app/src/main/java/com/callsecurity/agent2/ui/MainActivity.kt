971bbf5824016825ed1786c9c3ecd69566a6adb8
Dairon Rodriguez <dairolrpichardo90@yahoo.com>
2025-12-07 16:45:52 -0500

    Update MainActivity.kt

--- app/src/main/java/com/callsecurity/agent2/ui/MainActivity.kt
+++ app/src/main/java/com/callsecurity/agent2/ui/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.callsecurity.agent2.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
 import com.callsecurity.agent2.R
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnPermission = findViewById<MaterialButton>(R.id.btnPermission)
+
+        btnPermission.setOnClickListener {
+            val intent = Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
     }
 }
