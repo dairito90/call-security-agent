2ba8cad19c5e24a971fb5249e69a40c211e2c73a
Dairon Rodriguez <dairolrpichardo90@yahoo.com>
2025-12-09 13:26:05 -0500

    Update MainActivity.kt

--- app/src/main/java/com/callsecurity/agent2/ui/MainActivity.kt
+++ app/src/main/java/com/callsecurity/agent2/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.callsecurity.agent2.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.callsecurity.agent2.R
 
 class MainActivity : AppCompatActivity() {
