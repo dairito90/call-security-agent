58302f5a4a636b311d2a66d99527eb283ba30e26
Dairon Rodriguez <dairolrpichardo90@yahoo.com>
2025-12-09 13:14:37 -0500

    Update CallSecurityApplication.kt

--- app/src/main/java/com/callsecurity/agent2/CallSecurityApplication.kt
+++ app/src/main/java/com/callsecurity/agent2/CallSecurityApplication.kt
@@ -1,12 +1,5 @@
 package com.callsecurity.agent2
 
 import android.app.Application
-import android.util.Log
 
-class CallSecurityApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        Log.i("CallSecurityApp", "Application started")
-    }
-}
+class CallSecurityApplication : Application()
